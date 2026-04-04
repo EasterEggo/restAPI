f3f8416f7d7f04fa75b0b1a8df39290622a12c41
Paulo <phsb0607@gmail.com>
2026-04-04 17:42:49 -0300

    modularized app.js

--- src/app.js
+++ src/app.js
@@ -11,65 +11,12 @@ app.use(
   }),
 );
 const port = process.env.PORT
-//postgres
-const pg = require('pg')
-const { Pool } = pg
-const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASS,
-  host: process.env.PGHOST,
-  database: process.env.PGDB
-})
 
-async function viewTable(table, param, value) {
-  const tab = table.toString()
-  const par = param.toString()
-  const val = value.toString()
-  const query = param === undefined ? `SELECT * FROM ${tab}` : `SELECT * FROM ${tab} WHERE ${par} = ${val}`
-  const data = await pool.query(query)
-  return data
-}
+//routes for cnpq api endpoint
+const cnpq = require('./routes/cnpqroutes')
 
-async function insertCnpq(areas, subareas) {
-  const sub = subareas.map((item) => item.toString())
-  const query = {
-    text: 'INSERT INTO cnpq(nome, subareas) VALUES($1, $2)',
-    values: [areas.toString(), sub]
-  }
-  const res = await pool.query(query)
-  return res
-}
-async function deleteFrom(table, value) {
-  const query = {
-    text: `DELETE FROM ${table.toString()} WHERE id = $1`,
-    values: [Number(value)]
-  }
-  let del = await pool.query(query)
-  return del
-}
+app.use('/v1/cnpq', cnpq)
 
-app.get('/v1/cnpq', (req, res) => {
-  viewTable('cnpq')
-    .then(result => res.json(result.rows))
-})
-app.get('/v1/cnpq/:id', (req, res) => {
-  viewTable('cnpq', 'id', req.params.id)
-    .then((result) => res.json(result.rows[0]))
-})
-app.post('/v1/cnpq', (req, res) => {
-  const { nome, subarea } = req.body
-  if (nome == null || subarea == null) {
-    res.send("MISSING DATA")
-    return console.error("ERROR:MISSING DATA")
-  }
-  insertCnpq(nome, subarea)
-    .then((result) => res.json(result.rows))
-})
-
-app.delete('/v1/cnpq/:id', (req, res) => {
-  deleteFrom('cnpq', req.params.id)
-    .then((result) => res.send(result.rowCount === 0 ? "ID not found on database" : `Row with ID ${req.params.id} deleted successfully`))
-})
 
 app.listen(port, () => {
   console.log(`server running at port ${port}`)
